2437ab0efd7c3876323f5ae069e5caeb9bd04ca2
unrenamed <renkonazbka@gmail.com>
2022-08-03 23:10:50 +0300

    feat: enhance maze widget to adjust to the window size

--- src/app/widgets/maze_container.rs
+++ src/app/widgets/maze_container.rs
@@ -13,7 +13,7 @@ use crate::app::{
 pub struct MazeContainer<'a> {
     pub block: Option<Block<'a>>,
     pub grid: &'a Grid,
-    pub stack: &'a Vec<Pos>,
+    pub highlights: &'a Vec<Pos>,
 }
 
 impl<'a> Widget for MazeContainer<'a> {
@@ -36,10 +36,10 @@ impl<'a> Widget for MazeContainer<'a> {
 }
 
 impl<'a> MazeContainer<'a> {
-    pub fn new(grid: &'a Grid, stack: &'a Vec<Pos>) -> Self {
+    pub fn new(grid: &'a Grid, highlights: &'a Vec<Pos>) -> Self {
         MazeContainer {
             block: None,
-            stack,
+            highlights,
             grid,
         }
     }
@@ -50,85 +50,128 @@ impl<'a> MazeContainer<'a> {
     }
 
     pub fn display_grid(&mut self, area: Rect, buf: &mut Buffer) {
-        let x_margin = 3u16;
-        let y_margin = 5u16;
+        let grow_factor = self.get_grid_grow_factor(area, self.grid);
+        let (x_margin, y_margin) = self.get_grid_margins(grow_factor, area, self.grid);
         let grid_width = self.grid.width() as u16;
         let grid_height = self.grid.height() as u16;
+        let maze_width = grid_width * grow_factor * 2;
+        let maze_height = grid_height * grow_factor;
 
-        buf.get_mut(x_margin, y_margin)
-            .set_fg(Color::Green)
-            .set_symbol(" ");
-        for x in 1..=grid_width * 2 - 1 {
-            buf.get_mut(x_margin + x as u16, y_margin)
-                .set_fg(Color::Green)
-                .set_symbol("_");
+        self.add_passage((x_margin, y_margin), None, buf);
+
+        // draw top border
+        for x in 0..maze_width - 1 {
+            self.add_horizontal_wall((x_margin + x as u16 + 1, y_margin), None, buf);
         }
 
-        for (y, row) in self.grid.cells().iter().enumerate() {
-            buf.get_mut(x_margin, y_margin + y as u16 + 1)
-                .set_fg(Color::Green)
-                .set_symbol("|");
+        for y in 0..maze_height {
+            self.add_vertical_wall((x_margin, y_margin + y as u16 + 1), buf);
 
-            for (x, cell) in row.iter().enumerate() {
-                let walls = self.grid.get_cell((x, y)).get_walls();
+            for x in 0..maze_width {
+                // X coordinate including maring on the axis X
+                let nx = x_margin + x as u16 + 1;
+                // Y coordinate including maring on the axis Y
+                let ny = y_margin + y as u16 + 1;
+                // X coordinate of a cell in the grid
+                let cx = (x as f64 / grow_factor as f64 / 2 as f64).floor() as usize;
+                // Y coordinate of a cell in the grid
+                let cy = (y as f64 / grow_factor as f64).floor() as usize;
+                // Indicates if a row is a last row of a grid cell
+                let is_last_row = (y as f64 + 1.0) / grow_factor as f64 == cy as f64 + 1.0;
+                // Indicates if a column is a last column of a grid cell
+                let is_last_col =
+                    (x as f64 + 1.0) / grow_factor as f64 / 2 as f64 == cx as f64 + 1.0;
 
-                // draw southern wall
-                if walls.carved(Pole::S) {
-                    if self.stack.contains(&(x, y)) {
-                        buf.get_mut(x_margin + x as u16 * 2 + 1, y_margin + y as u16 + 1)
-                            .set_bg(Color::Red)
-                            .set_symbol(" ");
-                    } else {
-                        buf.get_mut(x_margin + x as u16 * 2 + 1, y_margin + y as u16 + 1)
-                            .set_bg(Color::Reset)
-                            .set_symbol(" ");
-                    }
-                } else {
-                    if self.stack.contains(&(x, y)) {
-                        buf.get_mut(x_margin + x as u16 * 2 + 1, y_margin + y as u16 + 1)
-                            .set_fg(Color::Green)
-                            .set_bg(Color::Red)
-                            .set_symbol("_");
-                    } else {
-                        buf.get_mut(x_margin + x as u16 * 2 + 1, y_margin + y as u16 + 1)
-                            .set_fg(Color::Green)
-                            .set_bg(Color::Reset)
-                            .set_symbol("_");
-                    }
-                }
+                let walls = self.grid.get_cell((cx, cy)).get_walls();
 
-                // draw eastern wall
-                if walls.carved(Pole::E) {
-                    let next_cell_walls = self.grid.get_cell((x + 1, y)).get_walls();
-                    if walls.carved(Pole::S) || next_cell_walls.carved(Pole::S) {
-                        if self.stack.contains(&(x, y)) {
-                            buf.get_mut(x_margin + x as u16 * 2 + 2, y_margin + y as u16 + 1)
-                                .set_bg(Color::Red)
-                                .set_symbol(" ");
+                match (is_last_row, is_last_col) {
+                    (false, false) => self.add_passage((nx, ny), Some((cx, cy)), buf),
+                    (false, true) => {
+                        if walls.carved(Pole::E) {
+                            self.add_passage((nx, ny), Some((cx, cy)), buf);
                         } else {
-                            buf.get_mut(x_margin + x as u16 * 2 + 2, y_margin + y as u16 + 1)
-                                .set_bg(Color::Reset)
-                                .set_symbol(" ");
-                        }
-                    } else {
-                        if self.stack.contains(&(x, y)) {
-                            buf.get_mut(x_margin + x as u16 * 2 + 2, y_margin + y as u16 + 1)
-                                .set_fg(Color::Green)
-                                .set_bg(Color::Red)
-                                .set_symbol("_");
-                        } else {
-                            buf.get_mut(x_margin + x as u16 * 2 + 2, y_margin + y as u16 + 1)
-                                .set_fg(Color::Green)
-                                .set_bg(Color::Reset)
-                                .set_symbol("_");
+                            self.add_vertical_wall((nx, ny), buf);
                         }
                     }
-                } else {
-                    buf.get_mut(x_margin + x as u16 * 2 + 2, y_margin + y as u16 + 1)
-                        .set_fg(Color::Green)
-                        .set_symbol("|");
+                    (true, false) => {
+                        if walls.carved(Pole::S) {
+                            self.add_passage((nx, ny), Some((cx, cy)), buf);
+                        } else {
+                            self.add_horizontal_wall((nx, ny), Some((cx, cy)), buf);
+                        }
+                    }
+                    (true, true) => {
+                        if walls.carved(Pole::E) {
+                            if walls.carved(Pole::S)
+                                || self.next_cell_carved_south(cx, cy, self.grid)
+                            {
+                                self.add_passage((nx, ny), Some((cx, cy)), buf);
+                            } else {
+                                self.add_horizontal_wall((nx, ny), Some((cx, cy)), buf);
+                            }
+                        } else {
+                            self.add_vertical_wall((nx, ny), buf);
+                        }
+                    }
                 }
             }
         }
+    }
+
+    fn get_grid_grow_factor(&self, area: Rect, grid: &Grid) -> u16 {
+        let margin = 2.0;
+
+        let grid_width = grid.width() as f64 * 2.0;
+        let grid_height = grid.height() as f64;
+        let width_factor = (area.width as f64 / (grid_width + margin)).floor().max(1.0);
+        let height_factor = (area.height as f64 / (grid_height + margin))
+            .floor()
+            .max(1.0);
+
+        width_factor.min(height_factor) as u16
+    }
+
+    fn get_grid_margins(&self, grow_factor: u16, area: Rect, grid: &Grid) -> (u16, u16) {
+        let grid_width = grid.width() as u16 * 2u16 * grow_factor;
+        let grid_height = grid.height() as u16 * grow_factor;
+
+        let x_margin = (area.width - grid_width) / 2;
+        let y_margin = (area.height - grid_height) / 2;
+
+        (x_margin + area.left() - 1, y_margin + area.top() - 1)
+    }
+
+    fn add_vertical_wall(&self, (x, y): (u16, u16), buf: &mut Buffer) {
+        buf.get_mut(x, y).set_fg(Color::Green).set_symbol("|");
+    }
+
+    fn add_horizontal_wall(&self, (x, y): (u16, u16), cell_pos: Option<Pos>, buf: &mut Buffer) {
+        let cell = buf.get_mut(x, y);
+        cell.set_fg(Color::Green).set_symbol("_");
+
+        if let Some((cx, cy)) = cell_pos {
+            if self.highlights.contains(&(cx, cy)) {
+                cell.set_bg(Color::Red);
+            }
+        }
+    }
+
+    fn add_passage(&self, (x, y): (u16, u16), cell_pos: Option<Pos>, buf: &mut Buffer) {
+        let cell = buf.get_mut(x, y);
+        cell.set_fg(Color::Green).set_symbol(" ");
+
+        if let Some((cx, cy)) = cell_pos {
+            if self.highlights.contains(&(cx, cy)) {
+                cell.set_bg(Color::Red);
+            }
+        }
+    }
+
+    fn next_cell_carved_south(&self, cx: usize, cy: usize, grid: &Grid) -> bool {
+        if cx + 1 >= grid.height() {
+            return false;
+        }
+        let walls = grid.get_cell((cx + 1, cy)).get_walls();
+        walls.carved(Pole::S)
     }
 }
