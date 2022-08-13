bb43d1f1a5493ac692a5ad9197f45fc6884a6091
unrenamed <renkonazbka@gmail.com>
2022-08-13 22:57:18 +0300

    feat: add cli options valitation

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ use argh::FromArgs;
 use eyre::Result;
 use std::time::Duration;
 
+static MAZE_MIN_WIDTH: usize = 1;
+static MAZE_MIN_HEIGHT: usize = 1;
+static MAZE_MAX_WIDTH: usize = 45;
+static MAZE_MAX_HEIGHT: usize = 45;
+
 #[derive(Debug, FromArgs)]
 #[argh(description = "Generate mazes")]
 struct Cli {
@@ -13,17 +18,38 @@ struct Cli {
     #[argh(option, short = 't', default = "33")]
     tick_rate: u64,
     /// grid width in characters. defaults to: 15
-    #[argh(option, short = 'w', default = "15")] // todo: define max width
+    #[argh(option, short = 'w', default = "15")]
     width: usize,
     /// grid height in characters. defaults to: 10
-    #[argh(option, short = 'h', default = "10")] // todo: define max height
+    #[argh(option, short = 'h', default = "10")]
     height: usize,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let cli: Cli = argh::from_env();
+    validate_cli_options(&cli);
+
     let tick_rate = Duration::from_millis(cli.tick_rate);
     terminal::run(tick_rate, cli.width, cli.height).await?;
+
     Ok(())
+}
+
+fn validate_cli_options(cli: &Cli) {
+    if cli.width < MAZE_MIN_WIDTH {
+        panic!("Maze width must take at least {} cell.", MAZE_MIN_WIDTH);
+    }
+
+    if cli.height < MAZE_MIN_HEIGHT {
+        panic!("Maze height must take at least {} cell.", MAZE_MIN_HEIGHT);
+    }
+
+    if cli.width > MAZE_MAX_WIDTH {
+        panic!("Maze width can not be greater than {} cells.", MAZE_MAX_WIDTH);
+    }
+
+    if cli.height > MAZE_MAX_HEIGHT {
+        panic!("Maze height can not be greater than {} cells.", MAZE_MAX_HEIGHT);
+    }
 }
